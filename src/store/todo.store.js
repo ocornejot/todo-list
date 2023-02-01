63131d954d08e237f8b9aebb608cf10d31c66821
ocornejo <ocornejo@arkusnexus.com>
2023-01-31 21:34:08 -0600

    FH Course: Save state with localStorage.

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -18,11 +18,20 @@ const state = {
 }
 
 const initStore = () => {
+  loadStore();
   console.log('initStore', {state});
 }
 
 const loadStore = () => {
-  throw new Error('Not implemented');
+  if ( !localStorage.getItem('state') ) return;
+
+  const { todos = [], filter = Filters.All } = JSON.parse( localStorage.getItem('state') );
+  state.todos = todos;
+  state.filter = filter;
+}
+
+const saveStateToLocalStorage = () => {
+  localStorage.setItem('state', JSON.stringify(state) );
 }
 
 /**
@@ -51,6 +60,7 @@ const getTodos = ( filter = Filters.All ) => {
 const addTodo = ( description ) => {
   if ( !description ) throw new Error('Description is required');
   state.todos.push( new Todo( description ) );
+  saveStateToLocalStorage();
 }
 
 /**
@@ -59,10 +69,12 @@ const addTodo = ( description ) => {
  */
 const deleteTodo = ( todoId ) => {
   state.todos = state.todos.filter( todo => todo.id !== todoId );
+  saveStateToLocalStorage();
 }
 
 const deleteCompleted = () => {
   state.todos = state.todos.filter( todo => !todo.done );
+  saveStateToLocalStorage();
 }
 
 /**
@@ -76,6 +88,7 @@ const toggleTodo = ( todoId ) => {
 
     return todo;
   });
+  saveStateToLocalStorage();
 }
 
 /**
